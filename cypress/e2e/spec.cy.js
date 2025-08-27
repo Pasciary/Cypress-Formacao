66db88b4b7a55df4cc104512a718ba667196d0e1
João Pachere <jppachere.fmoura@gmail.com>
2025-08-27 08:48:18 -0300

    Implementacao, Cadastro com sucesso usuário.

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Joaquim Silve');
+    cy.get('input[name="email"]').type('joaquim@gmail.com');
+    cy.get('input[name="password"]').type('Senha1234');
+    cy.get('input[name="confirm_password"]').type('Senha1234');
+    cy.contains('button', 'Cadastrar').click();
+  })
+})
