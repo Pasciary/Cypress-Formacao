c9c922bb43b182bbb8afe5af8d98607efb220154
João Pachere <jppachere.fmoura@gmail.com>
2025-08-28 20:43:19 -0300

    Conhecendo Flaky Tests.

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,5 +1,6 @@
 describe('API Adopet', () => {
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmE3YTFjZC1lMTc2LTRmNTQtOWYyMS0zNjk4OTdiMjUzYzIiLCJhZG9wdGVyTmFtZSI6IkpvYXF1aW0gU2lsdmUiLCJpYXQiOjE3NTY0MjIxMDgsImV4cCI6MTc1NjY4MTMwOH0.DwpBES0d2UI0TWss0DwfKhnY5W-hgfXmTSzefeyhQTc'
+    const tempoEsperado = Math.random() * 1000 // Test Flaky, é um teste que oscila, sem ser feito algo.
+    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmE3YTFjZC1lMTc2LTRmNTQtOWYyMS0zNjk4OTdiMjUzYzIiLCJhZG9wdGVyTmFtZSI6IkpvYXF1aW0gU2lsdmUiLCJpYXQiOjE3NTY0MjQwODcsImV4cCI6MTc1NjY4MzI4N30.v-3oqQ4cpkGglO1wJCBAyTHutmnxG2brE_O49uumwUI'
 
     it('Mensagens da API', () => {
         cy.request({
@@ -10,6 +11,7 @@ describe('API Adopet', () => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
+            expect(res.duration).to.be.lte(tempoEsperado) // usando o expect durantion com o tempoEsperado gera um Flaky Test.
         })
 
     })
